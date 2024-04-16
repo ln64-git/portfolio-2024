7a7a38b370969b4c943756ff75c27b06645a8c4a
Lucian <lucian.io@outlook.com>
2024-04-16 05:27:34 -0400

    now about page is responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,63 +12,63 @@ export default function AboutPage() {
   return (
     <div id="About" className=" mx-auto  max-w-[650px]   rounded-lg">
       <Header />
-      <div className="">
-        <div className="flex items-center justify-center  ">
-          <div className="min-w-[260px] p-4 flex flex-col justify-center ">
-            <div className="text-2xl">{name}</div>
-            <div className="text-lg">
-              <ul>
-                {info.map((element, index) => (
-                  <li key={index}>{element}</li>
+      <div className=" max-w-[450px] mx-auto flex items-center justify-around   ">
+        <div className="py-4 flex flex-col justify-center ">
+          <div className="text-2xl">{name}</div>
+          <div className="text-lg">
+            <ul>
+              {info.map((element, index) => (
+                <li key={index}>{element}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="flex items-center justify-center min-w-[100px]">
+          <Image
+            src="/lucian.jpg"
+            alt="Lucian"
+            width={100}
+            height={100}
+            className=""
+          />
+        </div>
+      </div>
+      <div
+        className={`${
+          isMobile
+            ? "max-w-[450px] my-4 mx-auto px-6"
+            : " max-w-[450px] my-4 mx-auto"
+        }`}
+      >
+        {summary}
+      </div>
+      <div>
+        <div className="text-center flex max-w-[500px] mx-auto ">
+          <div className=" py-4 ">
+            <div className="flex ">
+              <div className=" p-2 w-1/3">Front-End</div>
+              <ul className="flex flex-wrap w-2/3">
+                {skills1.map((skill, index) => (
+                  <li key={index} className="m-2">
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex ">
+              <div className=" p-2 w-1/3">Back-End</div>
+              <ul className="flex flex-wrap w-2/3">
+                {skills2.map((skill, index) => (
+                  <li key={index} className="m-2">
+                    {skill}
+                  </li>
                 ))}
               </ul>
             </div>
           </div>
-          <div className="flex items-center justify-center min-w-[100px]">
-            <Image
-              src="/lucian.jpg"
-              alt="Lucian"
-              width={100}
-              height={100}
-              className=""
-            />
-          </div>
         </div>
-        <div
-          className={`${
-            isMobile
-              ? "max-w-[450px] my-4 mx-auto px-6"
-              : " max-w-[450px] my-4 mx-auto"
-          }`}
-        >
-          {summary}
-          <div className=" text-center flex">
-            <div className=" py-4">
-              <div className="flex ">
-                <div className=" p-2 w-[300px]">Front-End</div>
-                <ul className="flex flex-wrap">
-                  {skills1.map((skill, index) => (
-                    <li key={index} className="m-2">
-                      {skill}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="flex ">
-                <div className=" p-2 w-[300px]">Back-End</div>
-                <ul className="flex flex-wrap">
-                  {skills2.map((skill, index) => (
-                    <li key={index} className="m-2">
-                      {skill}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <Fireflies />
       </div>
+      <Fireflies />
     </div>
   );
 }
