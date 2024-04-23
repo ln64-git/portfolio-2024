ea080ca38db1fca5ff459a0186c9c82f6eb889be
Luke <lucian.io@outlook.com>
2024-04-23 05:54:45 -0400

    cleaner animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
 
   const handleButtonClick = (route: string) => {
     setNextPath(route === "Home" ? "/" : `/${route.toLowerCase()}`);
-    console.log(route);
   };
 
   return (
--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -19,12 +19,10 @@ export default function Provider({
   return (
     <AnimatePresence mode="wait" initial={false}>
       <motion.div
-        key={nextPath} // Use nextPath as the key
+        key={nextPath}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
         transition={{ duration: 0.5 }}
-        
         onAnimationEnd={() => {
           router.push(nextPath);
         }}
