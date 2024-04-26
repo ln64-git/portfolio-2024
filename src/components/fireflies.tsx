cad7adaccaa51a6fea409308b52e98a15f7af5a1
Luke <lucian.io@outlook.com>
2024-04-26 04:52:28 -0400

    fixed dead pixels with fireflies

--- src/components/fireflies.tsx
+++ src/components/fireflies.tsx
@@ -1,7 +1,7 @@
 "use client";
 import "../utils/background.scss";
 export default function Fireflies() {
-  const fireflies = Array.from({ length: 45 }, (_, index) => (
+  const fireflies = Array.from({ length: 25 }, (_, index) => (
     <div key={index} className="firefly"></div>
   ));
   return <>{fireflies}</>; // Return JSX directly
